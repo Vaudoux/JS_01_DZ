2e0eb3b7f9ceeb10518e974b132dce1c0e0992b3
Ilya <ildorf@list.ru>
2023-07-28 19:56:08 +0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//Задание 1
+// температура в градусах по Цельсию
+let temperatureC = 30;
+// температура в градусах по Фаренгейту
+let temperatureF = (9 / 5) * temperatureC + 32;
+
+console.log(`Температура ${temperatureC} градусов по Цельсию`);
+console.log(`Температура ${temperatureF} градусов по Фаренгейту`);
+
+//Задание 2
+let userName = 'Илья';
+let admin = userName;
+console.log(admin);
